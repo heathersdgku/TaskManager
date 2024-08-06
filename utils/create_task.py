79d5b4c0694661e4b023bb723fc78f9cc252e076
heathersdgku <heatherchoe@gmail.com>
2024-08-06 16:39:22 -0700

    Class 4

--- utils/create_task.py
+++ utils/create_task.py
@@ -5,12 +5,12 @@ URL = "http://127.0.0.1:5000/tasks"
 def create_task(name, summary, description):
     task_data = {
         "name": name,
-        "summary": summary, 
+        "summary": summary,
         "description": description
     }
     response = requests.post(URL, json=task_data)
     if response.status_code == 204:
-        print ("Task created successfully")
+        print ("Task successfully created")
     else:
         print ("Task creation failed")
 
